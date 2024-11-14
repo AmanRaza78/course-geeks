70d48d9bd806e43ab27ed72d5fe9a38ec827c610
AmanRaza78 <razakhanaman9@gmail.com>
2024-11-14 12:27:55 +0530

    authentication and landing page ui done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import CallToAction from "@/components/landing-ui/call-to-action"
+import FeatureSection from "@/components/landing-ui/feature-section"
+import HeroSection from "@/components/landing-ui/hero-section"
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-1">
+        <HeroSection/>
+        <FeatureSection/>
+        <CallToAction/>
+      </main>
+      
     </div>
-  );
+  )
 }
