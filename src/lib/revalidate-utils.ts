e9ea24eae7cda1fe336c7f0c5d77c12b4dff003a
AmanRaza78 <razakhanaman9@gmail.com>
2024-11-24 20:51:41 +0530

    created the report functionality

---
+++ src/lib/revalidate-utils.ts
@@ -0,0 +1,7 @@
+"use server";
+import { revalidatePath } from "next/cache";
+
+export async function RevalidateUtils(reviewId: string ) {
+  "use server";
+  revalidatePath(`/review/${reviewId}`);
+}
